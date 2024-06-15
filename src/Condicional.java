3e1eaa7e6637b0c87e360e00840ee42ca3eeece2
Ricardo <ricardofh33@gmail.com>
2024-06-15 10:05:54 -0300

    primeiro curso

---
+++ src/Condicional.java
@@ -0,0 +1,20 @@
+public class Condicional {
+    public static void main(String[] args) {
+        int anoDeLancamento = 2022;
+        boolean incluidoNoPlano = false;
+        double notaDoFile = 8.1;
+        String tipoPlano = "normal";
+
+        if (anoDeLancamento >= 2022){
+            System.out.println("Lançamento que os clientes estão curtindo");
+        } else {
+            System.out.println("Filme retro que vale a pena assistir");
+        }
+
+        if (incluidoNoPlano && tipoPlano.equals("plus")) {
+            System.out.println("Filme liberado");
+        } else {
+            System.out.println("Deve pagar locação");
+        }
+    }
+}
